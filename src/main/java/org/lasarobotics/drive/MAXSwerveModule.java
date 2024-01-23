08271fbd4e4efb43a1ddbeb0e56f7d271beade25
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2024-01-23 16:08:46 -0600

    Code cleanup

--- src/main/java/org/lasarobotics/drive/MAXSwerveModule.java
+++ src/main/java/org/lasarobotics/drive/MAXSwerveModule.java
@@ -151,15 +151,16 @@ public class MAXSwerveModule implements AutoCloseable {
   public MAXSwerveModule(Hardware swerveHardware, ModuleLocation location, GearRatio driveGearRatio,
                          Measure<Distance> wheelbase, Measure<Distance> trackWidth, Measure<Time> autoLockTime,
                          Measure<Current> driveMotorCurrentLimit, double slipRatio) {
-    var motorKind = swerveHardware.driveMotor.getKind();
-    int encoderTicksPerRotation = motorKind.equals(MotorKind.NEO) ? GlobalConstants.NEO_ENCODER_TICKS_PER_ROTATION : GlobalConstants.VORTEX_ENCODER_TICKS_PER_ROTATION;
+    int encoderTicksPerRotation = swerveHardware.driveMotor.getKind().equals(MotorKind.NEO)
+      ? GlobalConstants.NEO_ENCODER_TICKS_PER_ROTATION
+      : GlobalConstants.VORTEX_ENCODER_TICKS_PER_ROTATION;
     DRIVE_MOTOR_CURRENT_LIMIT = (int)driveMotorCurrentLimit.in(Units.Amps);
     DRIVE_TICKS_PER_METER =
       (encoderTicksPerRotation * driveGearRatio.value)
       * (1 / (DRIVE_WHEEL_DIAMETER_METERS * Math.PI));
     DRIVE_METERS_PER_TICK = 1 / DRIVE_TICKS_PER_METER;
     DRIVE_METERS_PER_ROTATION = DRIVE_METERS_PER_TICK * encoderTicksPerRotation;
-    DRIVE_MAX_LINEAR_SPEED = (motorKind.getMaxRPM() / 60) * DRIVE_METERS_PER_ROTATION * DRIVETRAIN_EFFICIENCY;
+    DRIVE_MAX_LINEAR_SPEED = (swerveHardware.driveMotor.getKind().getMaxRPM() / 60) * DRIVE_METERS_PER_ROTATION * DRIVETRAIN_EFFICIENCY;
 
     this.m_driveMotor = swerveHardware.driveMotor;
     this.m_rotateMotor = swerveHardware.rotateMotor;
