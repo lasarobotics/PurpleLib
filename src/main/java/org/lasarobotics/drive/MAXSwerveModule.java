306eb9503f22f31ac827d6525c35556b4b121fb2
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2024-07-17 16:14:22 -0500

    Move module location to separate class

--- src/main/java/org/lasarobotics/drive/MAXSwerveModule.java
+++ src/main/java/org/lasarobotics/drive/MAXSwerveModule.java
@@ -58,23 +58,6 @@ public class MAXSwerveModule implements AutoCloseable {
     }
   }
 
-  /** Module location */
-  public enum ModuleLocation {
-    LeftFront(0, Rotation2d.fromRadians(-Math.PI / 2)),
-    RightFront(1, Rotation2d.fromRadians(+0.0)),
-    LeftRear(2, Rotation2d.fromRadians(+Math.PI)),
-    RightRear(3, Rotation2d.fromRadians(+Math.PI / 2));
-
-    /** Module index */
-    public final int index;
-    /** Module orientation chassis offset */
-    public final Rotation2d offset;
-    private ModuleLocation(int index, Rotation2d offset) {
-      this.index = index;
-      this.offset = offset;
-    }
-  }
-
   /**
    * MAXSwerve gear ratio
    */
