d427b9de8c536635fc5c2d7ee1acca5ba94d067a
aahFancyGun <yangfanchau2008@gmail.com>
2025-01-11 11:04:29 -0600

    revised version

--- src/main/java/org/lasarobotics/hardware/thethriftybot/ThriftyNova.java
+++ src/main/java/org/lasarobotics/hardware/thethriftybot/ThriftyNova.java
@@ -1,17 +1,19 @@
 package org.lasarobotics.hardware.thethriftybot;
 
+import java.util.List;
+
 import org.lasarobotics.hardware.LoggableHardware;
 import org.lasarobotics.hardware.PurpleManager;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
-import com.ctre.phoenix.ErrorCode;
-import com.thethriftybot.ThriftyNova;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.units.measure.Voltage;
 
-import java.util.*;
 
-
-public class ThriftybotMotor extends LoggableHardware {
+public class ThriftyNova extends LoggableHardware {
 
   public static class ID {
     public final String name;
@@ -24,25 +26,33 @@ public class ThriftybotMotor extends LoggableHardware {
     }
   }
 
+  private enum ControlType {
+    PERCENT,
+    POSITION,
+    VELOCITY,
+    VOLTAGE,
+    BOOLEAN
+  }
+
   @AutoLog
-  public static class ThriftybotInputs{
+  public static class ThriftyNovaInputs{
     public double position = 0.0;
     public double velocity = 0.0;
   }
-
-  private static ThriftyNova m_thrifty;
+  
+  private com.thethriftybot.ThriftyNova m_thrifty;
   private static final String VALUE_LOG_ENTRY = "/OutputValue";
   private static final String MODE_LOG_ENTRY = "/OutputMode";
   private static final String CURRENT_LOG_ENTRY = "/Current";
 
 
   private ID m_id;
-  private ThriftybotInputsAutoLogged m_inputs;
+  private ThriftyNovaInputsAutoLogged m_inputs;
 
-  public ThriftybotMotor(ID id){
+  public ThriftyNova(ID id) {
     this.m_id = id;
     this.m_thrifty = new com.thethriftybot.ThriftyNova(id.deviceID);
-    this.m_inputs = new ThriftybotInputsAutoLogged();
+    this.m_inputs = new ThriftyNovaInputsAutoLogged();
 
     periodic();
 
@@ -52,11 +62,11 @@ public class ThriftybotMotor extends LoggableHardware {
   /**
    * Log output values
    * @param value Value that was set
-   * @param mode The output mode to apply
+   * @param type value types
    */
-  public void logOutputs(double value) {
+  private void logOutputs(double value, ControlType type) {
     Logger.recordOutput(m_id.name + VALUE_LOG_ENTRY, value);
-    Logger.recordOutput(m_id.name + MODE_LOG_ENTRY, m_thrifty.toString());
+    Logger.recordOutput(m_id.name + MODE_LOG_ENTRY, type.name());
     Logger.recordOutput(m_id.name + CURRENT_LOG_ENTRY, getStatorCurrent());
   }
 
@@ -65,7 +75,7 @@ public class ThriftybotMotor extends LoggableHardware {
    *
    * @return Position of selected sensor (in raw sensor units).
    */
-  public double getPosition() {
+  private double getPosition() {
     return m_thrifty.getPosition();
   }
   
@@ -73,8 +83,9 @@ public class ThriftybotMotor extends LoggableHardware {
    * Sets position of PID controller
    * @param targetPosition
    */
-  public void setPosition(double targetPosition){
+  public void setPosition(double targetPosition) {
     m_thrifty.setPosition(targetPosition);
+    logOutputs(targetPosition, ControlType.POSITION);
   }
 
   /**
@@ -82,22 +93,22 @@ public class ThriftybotMotor extends LoggableHardware {
    * The slower that frame is sent, the slower the follower motor will update.
    * @param followerID - The Target ID to follow
    */
-  public void follow(int followerID){
+  public void follow(int followerID) {
     m_thrifty.follow(followerID);
   }
 
   /**
    * Calls every status/feedback getter that has a network table entry
    */
-  public void updateStatusNT(){
+  public void updateStatusNT() {
     m_thrifty.updateStatusNT();
   }
 
   /**
    * Updates the network tables for all ThriftyNova motors.
    */
-  public static void updateStatusNTGlobal(){
-    m_thrifty.updateStatusNTGlobal();
+  public static void updateStatusNTGlobal() {
+    com.thethriftybot.ThriftyNova.updateStatusNTGlobal();
   }
 
 
@@ -107,14 +118,14 @@ public class ThriftybotMotor extends LoggableHardware {
    * @return selected sensor (in raw sensor units) per 100ms.
    * See Thriftybot-Documentation for how to interpret.
    */
-  public double getVelocity() {
+  private double getVelocity() {
     return m_thrifty.getVelocity();
   }
 
   /**
    * Update sensor input readings
    */
-  public void updateInputs() {
+  private void updateInputs() {
     m_inputs.position = getPosition();
     m_inputs.velocity = getVelocity();
   }
@@ -130,7 +141,7 @@ public class ThriftybotMotor extends LoggableHardware {
    * @return Latest sensor data
    */
   @Override
-  public ThriftybotInputsAutoLogged getInputs() {
+  public ThriftyNovaInputsAutoLogged getInputs() {
     return m_inputs;
   }
 
@@ -138,7 +149,7 @@ public class ThriftybotMotor extends LoggableHardware {
    * Get device ID
    * @return Device ID
    */
-  public ID getID(){
+  public ID getID() {
     return m_id;
   }
 
@@ -151,7 +162,7 @@ public class ThriftybotMotor extends LoggableHardware {
    */
   public void setVelocity(double speed) {
     m_thrifty.setVelocity(speed);
-    logOutputs(speed);
+    logOutputs(speed, ControlType.VELOCITY);
   }
 
   
@@ -165,20 +176,21 @@ public class ThriftybotMotor extends LoggableHardware {
    */
   public void set(double value) {
     m_thrifty.set(value);
+    logOutputs(value, ControlType.PERCENT);
   }
 
   /**
    * Returns the last set point specified by the user in any control method. Percent output set point becomes zero.
    * @return the last set point specified by the user in any control method
    */
-  public double get(){
+  public double get() {
     return m_thrifty.get();
   }
 
   /**
    * @return the error to the last set point that was input, based on last used control mode (position or velocity), and encoder type (internal or quaderature).
    */
-  public double getClosedLoopError(){
+  public double getClosedLoopError() {
     return m_thrifty.getClosedLoopError();
   }
 
@@ -188,8 +200,9 @@ public class ThriftybotMotor extends LoggableHardware {
    * sets percent output form between 1.0 (full forward) to -1.0 (full backward), 0.0 = full stop
    * @param percentOutput
    */
-  public void setPercent(double percentOutput){
+  public void setPercent(double percentOutput) {
     m_thrifty.setPercent(percentOutput);
+    logOutputs(percentOutput, ControlType.PERCENT);
   }
 
 
@@ -197,8 +210,9 @@ public class ThriftybotMotor extends LoggableHardware {
    * 
    * @param value the maximum forward output, always between 0.0 and 1.0
    */
-  public void setMaxOutput(double value){
+  public void setMaxOutput(double value) {
     m_thrifty.setMaxOutput(value);
+    logOutputs(value, ControlType.PERCENT);
   }
 
   /**
@@ -206,8 +220,10 @@ public class ThriftybotMotor extends LoggableHardware {
    * @param fwdValue maximum forward output (range of 0.0 to 1.0)
    * @param bwdValue maximum backward output (range of -1.0 to 1.0)
    */
-  public void setMaxOutput(double fwdValue, double bwdValue){
+  public void setMaxOutput(double fwdValue, double bwdValue) {
     m_thrifty.setMaxOutput(fwdValue, bwdValue);
+    logOutputs(fwdValue, ControlType.PERCENT);
+    logOutputs(bwdValue, ControlType.PERCENT);
   }
 
   /**
@@ -215,8 +231,8 @@ public class ThriftybotMotor extends LoggableHardware {
    * @return an int that denotes Stator current which is the output current of the motor
    *            measured in amps
    */           
-  public int getStatorCurrent(){
-    return m_thrifty.getStatorCurrent();
+  public Current getStatorCurrent() {
+    return Units.Amps.of(m_thrifty.getStatorCurrent());
   }
 
   /**
@@ -224,8 +240,8 @@ public class ThriftybotMotor extends LoggableHardware {
    * @return an int that denotes the amount of current moving from supply
    *        measured in amps
    */
-  public int getSupplyCurrent(){
-    return m_thrifty.getSupplyCurrent();
+  public Current getSupplyCurrent() {
+    return Units.Amps.of(m_thrifty.getSupplyCurrent());
   }
 
   /**
@@ -233,7 +249,7 @@ public class ThriftybotMotor extends LoggableHardware {
    * @param encoderType encoder type to use
    *                consult Thriftybot documentation for additional information on encoder types
    */
-  public void useEncoderType(ThriftyNova.EncoderType encoderType){
+  public void useEncoderType(com.thethriftybot.ThriftyNova.EncoderType encoderType) {
     m_thrifty.useEncoderType(encoderType);
   }
 
@@ -241,21 +257,22 @@ public class ThriftybotMotor extends LoggableHardware {
    * @param rampDownTime time motor controller will take to go from 100% to idle
    *                input of 0.5 means the motor will 0.5 seconds to go from 100% to idle
    */
-  public void setRampDown(double rampDownTime){
-    m_thrifty.setRampDown(rampDownTime);
+  public void setRampDown(Time rampDownTime) {
+    m_thrifty.setRampDown(rampDownTime.in(Units.Seconds));
   }
 
   /**
    * @param rampUpTime time motor controller will take to go from idle to 100%
    *                input of 0.5 means the motor will 0.5 seconds to go from idle to 100%
    */
-  public void setRampUp(double rampUpTime){
-    m_thrifty.setRampUp(rampUpTime);
+  public void setRampUp(Time rampUpTime) {
+    m_thrifty.setRampUp(rampUpTime.in(Units.Seconds));
+   
   }
   /**
    * @param pidSlot Sets the pidslot to use for feedback control
    */
-  public void usePidSlot(ThriftyNova.PIDSlot pidSlot){
+  public void usePidSlot(com.thethriftybot.ThriftyNova.PIDSlot pidSlot) {
     m_thrifty.usePIDSlot(pidSlot);
   }
 
@@ -279,15 +296,16 @@ public class ThriftybotMotor extends LoggableHardware {
    *
    * @param outputVolts The voltage to output.
    */
-  public void setVoltage(double outputVolts) {
+  public void setVoltage(Voltage outputVolts) {
     m_thrifty.setVoltage(outputVolts);
+    logOutputs(outputVolts.in(Units.Volts), ControlType.VOLTAGE);
   }
 
   /**
    * gets voltage measurement
    */
-  public double getVoltage(){
-    return m_thrifty.getVoltage();
+  public Voltage getVoltage() {
+    return Units.Volts.of(m_thrifty.getVoltage());
   }
 
 
@@ -297,14 +315,14 @@ public class ThriftybotMotor extends LoggableHardware {
    * @param value
    *            value to limit.
    */
-  public void configPeakCurrentLimit(ThriftyNova.CurrentType currentType, double value) {
-    m_thrifty.setMaxCurrent(currentType, value); 
+  public void configPeakCurrentLimit(com.thethriftybot.ThriftyNova.CurrentType currentType, Current value) {
+    m_thrifty.setMaxCurrent(currentType, value.in(Units.Amps)); 
   }
 
   /**
    * @return arraylist of errors stored in error buffer
    */
-  public List<ThriftyNova.Error> getErrors(){
+  public List<com.thethriftybot.ThriftyNova.Error> getErrors() {
     return m_thrifty.getErrors();
   }
 
@@ -314,22 +332,22 @@ public class ThriftybotMotor extends LoggableHardware {
    * @return a boolean denoting if motor controller has incurred an error of specified type
    *        True = Error, False = No Error
    */
-  public boolean hasErrors(ThriftyNova.Error errorType){
+  public boolean hasErrors(com.thethriftybot.ThriftyNova.Error errorType) {
     return m_thrifty.hasError(errorType);
   }
 
   /**
    * gets ramp up time in seconds
    */
-  public double getRampUp(){
-    return m_thrifty.getRampUp();
+  public Time getRampUp() {
+    return Units.Seconds.of(m_thrifty.getRampUp());
   }
 
   /**
    * gets ramp down time in seconds
    */
-  public double getRampDown(){
-    return m_thrifty.getRampDown();
+  public Time getRampDown(){
+    return Units.Seconds.of(m_thrifty.getRampDown());
   }
 
   /**
@@ -337,20 +355,20 @@ public class ThriftybotMotor extends LoggableHardware {
    */
   public void stopMotor() {
     m_thrifty.stopMotor();
-    logOutputs(0.0);
+    logOutputs(0.0, ControlType.PERCENT);
   }
 
   /**
    * disables motor controller
    */
-  public void disable(){
+  public void disable() {
     m_thrifty.disable();
   }
 
   /**
    * Gets break mode status of motor
    */
-  public boolean getBrakeMode(){
+  public boolean getBrakeMode() {
     return m_thrifty.getBrakeMode();
   }
 
@@ -358,14 +376,14 @@ public class ThriftybotMotor extends LoggableHardware {
    * gets max forward output
    * 
    */
-  public double getMaxForwardOutput(){
+  public double getMaxForwardOutput() {
     return m_thrifty.getMaxForwardOutput();
   }
 
   /**
    * gets max reverse output
    */
-  public double getMaxReverseOutput(){
+  public double getMaxReverseOutput() {
     return m_thrifty.getMaxReverseOutput();
   }
 
@@ -374,54 +392,51 @@ public class ThriftybotMotor extends LoggableHardware {
    * @param revLimit - reverse position the motor will not go past
    * @param fwdLimit - forward position the motor will not go past
    */
-  public ThriftyNova setSoftLimits(double revLimit, double fwdLimit){
+  public void setSoftLimits(double revLimit, double fwdLimit) {
     m_thrifty.setSoftLimits(revLimit, fwdLimit);
-    return m_thrifty;
   }
 
   /**
    * enables soft limits on motor
    * @param enable - boolean variable if true, soft limits are enabled, disabled otherwise
    */
-  public ThriftyNova enableSoftLimits(boolean enable){
+  public void enableSoftLimits(boolean enable) {
     m_thrifty.enableSoftLimits(enable);
-    return m_thrifty;
   }
 
   /**
    * enable hard limits on motor
    * @param enable - boolean variable if true, soft limits are enabled, disabled otherwise
    */
-  public ThriftyNova enableHardLimits(boolean enable){
+  public void enableHardLimits(boolean enable) {
     m_thrifty.enableHardLimits(enable);
-    return m_thrifty;
   }
 
   /**
    * Gets maximum current limit
    */
-  public double getMaxCurrent(){
-    return m_thrifty.getMaxCurrent();
+  public Current getMaxCurrent() {
+    return Units.Amps.of(m_thrifty.getMaxCurrent());
   }
 
   /**
    * gets current type being used for limiting
    */
-  public ThriftyNova.CurrentType getCurrentType(){
+  public com.thethriftybot.ThriftyNova.CurrentType getCurrentType() {
     return m_thrifty.getCurrentType();
   }
 
   /**
    * gets forward soft limit
    */
-  public double getForwardSoftLimit(){
+  public double getForwardSoftLimit() {
     return m_thrifty.getForwardSoftLimit();
   }
 
   /**
    * gets reverse soft limit
    */
-  public double getReverseSoftLimit(){
+  public double getReverseSoftLimit() {
     return m_thrifty.getReverseSoftLimit();
   }
 
@@ -429,7 +444,7 @@ public class ThriftybotMotor extends LoggableHardware {
    * gets whether soft limits are enabled
    * @return boolean indicating whether or not soft limits are enabled
    */
-  public boolean areSoftLimitsEnabled(){
+  public boolean areSoftLimitsEnabled() {
     return m_thrifty.areSoftLimitsEnabled();
   }
 
@@ -437,48 +452,45 @@ public class ThriftybotMotor extends LoggableHardware {
    * gets whether hard limits are enabled
    * @return boolean indicating whether or not hard limits are enabled
    */
-  public boolean areHardLimitsEnabled(){
+  public boolean areHardLimitsEnabled() {
     return m_thrifty.areHardLimitsEnabled();
   }
 
   /**
    * Sets the absolute encoder offset.
    * @param offset integer that specifies the amount of offset 
-   * @return thriftynova object
    */
-  public ThriftyNova setAbsOffset(int offset){
+  public void setAbsOffset(int offset) {
     m_thrifty.setAbsOffset(offset);
-    return m_thrifty;
+    logOutputs(offset, ControlType.POSITION);
   }
 
   /**
    * gets absolute encoder offset
    */
-  public int getAbsOffset(){
+  public int getAbsOffset() {
     return m_thrifty.getAbsOffset();
   }
 
   /**
    * Zeros the absolute encoder reading at the current position
    */
-  public ThriftyNova zeroAbsEncoder(){
+  public void zeroAbsEncoder() {
     m_thrifty.zeroAbsEncoder();
-    return m_thrifty;
   }
 
   /**
    * Sets the temperature throttle enable state.
    * @param enable - boolean indicating whether or not this setting is enabled or not
    */
-  public ThriftyNova setTempThrottleEnable(boolean enable){
+  public void setTempThrottleEnable(boolean enable) {
     m_thrifty.setTempThrottleEnable(enable);
-    return m_thrifty;
   }
 
   /**
    * gets the temperature throttle enable state
    */
-  public boolean getTemptThrottleEnable(){
+  public boolean getTemptThrottleEnable() {
     return m_thrifty.getTempThrottleEnable();
   }
   
@@ -486,7 +498,7 @@ public class ThriftybotMotor extends LoggableHardware {
    * gets temperature of motor controller
    * @return integer that specifies temperature of motor controller
    */
-  public int getTemperature(){
+  public int getTemperature() {
     return m_thrifty.getTemperature();
   }
 
@@ -496,16 +508,15 @@ public class ThriftybotMotor extends LoggableHardware {
    * sets motor type
    * @param motorType specifies the motor type to be used
    */
-  public ThriftyNova setMotorType(ThriftyNova.MotorType motorType){
+  public void setMotorType(com.thethriftybot.ThriftyNova.MotorType motorType) {
     m_thrifty.setMotorType(motorType);
-    return m_thrifty;
   }
 
   /**
    * Returns the last set point specified by the user in any control method. Percent output set point becomes zero.
    * @return double that encodes the last set point specified by user in any controler method
    */
-  public double getSetPoint(){
+  public double getSetPoint() {
     return m_thrifty.getSetPoint();
   }
 
@@ -514,7 +525,7 @@ public class ThriftybotMotor extends LoggableHardware {
   /**
    * gets motor type
    */
-  public ThriftyNova.MotorType getMotorType(){
+  public com.thethriftybot.ThriftyNova.MotorType getMotorType() {
     return m_thrifty.getMotorType();
   }
 
@@ -522,39 +533,38 @@ public class ThriftybotMotor extends LoggableHardware {
    * sets voltage compensation value
    * @param vcomp the voltage compensation value
    */
-  public ThriftyNova setVoltageCompensation(double vcomp){
-    m_thrifty.setVoltageCompensation(vcomp);
-    return m_thrifty;
+  public void setVoltageCompensation(Voltage vcomp) {
+    m_thrifty.setVoltageCompensation(vcomp.in(Units.Volt));
+    logOutputs(vcomp.in(Units.Volts), ControlType.VOLTAGE);
   }
 
   /**
    * gets the voltage compensation value
    */
-  public double getVoltageCompensation(){
-    return m_thrifty.getVoltageCompensationEnabled();
+  public Voltage getVoltageCompensationEnabled() {
+    return Units.Volts.of(m_thrifty.getVoltageCompensationEnabled());
   }
 
   /**
    * sets external encoder
    * @param external is the external encoder to use
    */
-  public ThriftyNova setExternalEncoder(ThriftyNova.ExternalEncoder external){
+  public void setExternalEncoder(com.thethriftybot.ThriftyNova.ExternalEncoder external) {
     m_thrifty.setExternalEncoder(external);
-    return m_thrifty;
   }
   
   /**
    * sets encoder position
    * @param position
    */
-  public void setEncoderPosition(double position){
+  public void setEncoderPosition(double position) {
     m_thrifty.setEncoderPosition(position);
   }
 
   /**
    * resets motor controller to default state
    */
-  public void factoryReset(){
+  public void factoryReset() {
     m_thrifty.factoryReset();
   }
 
@@ -564,7 +574,7 @@ public class ThriftybotMotor extends LoggableHardware {
    * Sets NT logging
    * @param enable specifies whether this setting is enabled or not
    */
-  public void setNTLogging(boolean enable){
+  public void setNTLogging(boolean enable) {
     m_thrifty.setNTLogging(enable);
   }
 
@@ -572,7 +582,7 @@ public class ThriftybotMotor extends LoggableHardware {
 
 
   /**
-   * Closes the TalonSRX motor controller
+   * Closes the ThriftyNova motor controller
    */
   @Override
   public void close() {
