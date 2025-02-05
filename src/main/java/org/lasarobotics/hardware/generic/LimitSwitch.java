e0fa660e871e9abc54f4b66c3b79571ccdea6ac8
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2025-02-05 17:38:44 -0600

    Add binding callback as interrupt to limit switch

--- src/main/java/org/lasarobotics/hardware/generic/LimitSwitch.java
+++ src/main/java/org/lasarobotics/hardware/generic/LimitSwitch.java
@@ -4,12 +4,15 @@
 
 package org.lasarobotics.hardware.generic;
 
+import java.util.function.BiConsumer;
+
 import org.lasarobotics.hardware.LoggableHardware;
 import org.lasarobotics.hardware.PurpleManager;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.units.measure.Frequency;
+import edu.wpi.first.wpilibj.AsynchronousInterrupt;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /** Limit switch */
@@ -31,35 +34,19 @@ public class LimitSwitch extends LoggableHardware {
   }
 
   /**
-   * Switch polarity
-   */
-  public enum SwitchPolarity {
-    /** Normally open switch */
-    NORMALLY_OPEN {
-      @Override
-      public boolean getValue(boolean value) { return value; }
-    },
-    /** Normally closed switch */
-    NORMALLY_CLOSED {
-      @Override
-      public boolean getValue(boolean value) { return !value; }
-    };
-
-    public abstract boolean getValue(boolean value);
-  }
-
-  /**
    * Limit switch sensor inputs
    */
   @AutoLog
   public static class LimitSwitchInputs {
+    /**
+     * True when active for normally open or normally closed
+     */
     public boolean value = false;
   }
 
   private DigitalInput m_limitSwitch;
 
   private ID m_id;
-  private SwitchPolarity m_switchPolarity;
   private Frequency m_updateRate;
   private volatile LimitSwitchInputsAutoLogged m_inputs;
 
@@ -69,9 +56,8 @@ public class LimitSwitch extends LoggableHardware {
    * @param switchPolarity Switch polarity
    * @param updateRate Update rate of input from limit switch
    */
-  public LimitSwitch(LimitSwitch.ID id, SwitchPolarity switchPolarity, Frequency updateRate) {
+  public LimitSwitch(LimitSwitch.ID id, Frequency updateRate) {
     this.m_id = id;
-    this.m_switchPolarity = switchPolarity;
     this.m_updateRate = updateRate;
     this.m_limitSwitch = new DigitalInput(m_id.port);
     this.m_inputs = new LimitSwitchInputsAutoLogged();
@@ -88,7 +74,7 @@ public class LimitSwitch extends LoggableHardware {
    * Update sensor input readings
    */
   protected void updateInputs() {
-    m_inputs.value = m_switchPolarity.getValue(m_limitSwitch.get());
+    m_inputs.value = m_limitSwitch.get();
   }
 
   /**
@@ -111,6 +97,26 @@ public class LimitSwitch extends LoggableHardware {
   @Override
   public LimitSwitchInputsAutoLogged getInputs() {
     synchronized (m_inputs) { return m_inputs; }
+  }
+
+  /**
+   * Bind callback to rising and/or falling edge of limit switch signal as asynchronous interrupt
+   * <p>
+   * The first bool in the callback indicates the rising edge triggered the interrupt, the second
+   * bool is falling edge.
+   * <p>
+   * Interrupt is enabled by default
+   * @param callback Callback to run when interrupt is activated
+   * @param risingEdge Trigger on rising edge
+   * @param fallingEdge Trigger on falling edge
+   * @return Interrupt that was bound
+   */
+  public AsynchronousInterrupt bindInterrupt(BiConsumer<Boolean, Boolean> callback, boolean risingEdge, boolean fallingEdge) {
+    var interrupt = new AsynchronousInterrupt(m_limitSwitch, callback);
+    interrupt.setInterruptEdges(risingEdge, fallingEdge);
+    interrupt.enable();
+
+    return interrupt;
   }
 
   @Override
